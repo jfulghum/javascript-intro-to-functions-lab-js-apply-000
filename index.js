531b99b296e2e45d8ad048b13d0eb15a1dce4418
Johanna Fulghum <johanna.fulghum@gmail.com>
2017-10-11 17:27:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ function shout(string){
 function whisper(string){
   return string.toLowerCase();
 }
+
+function logwhisper(string){
+  console.log(string.toUpperCase());
+}
+
+function sayHiToGrandma(string){
+  if (string.toLowerCase() == string){
+    return "I can't hear you!"
+  } else if (string = "I love you, Grandma."){
+    return "I love you, too."
+  } else {
+    return "YES INDEED!"
+  }
+}
